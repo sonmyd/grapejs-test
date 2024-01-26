0c24549c442471d7b317a0bd740a5c257cd5c65a
Santiago Bedoya <santiago@Elrepapu-Mac.local>
2024-01-25 20:39:37 -0500

    added fakeData

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,32 @@ import { useEffect, useState } from 'react';
 import gjsPresentWebpage from 'grapesjs-preset-webpage';
 import grapesjsBlocksBasic from 'grapesjs-blocks-basic';
 import gjsForms from 'grapesjs-plugin-forms';
+import data from "./dataFake.json";
 
 function App() {
   const [editor, setEditor] = useState(null);
   useEffect(() => {
+    if (editor != null) {
+      const projectData = editor.getProjectData();
+      console.log(projectData);
+      editor.loadProjectData(data);
+    }
+  }, [editor]);
+
+
+  useEffect(() => {
     const editor = grapejs.init({
       container: '#editor',
-      plugins: [grapesjsBlocksBasic,gjsPresentWebpage,gjsForms],
+      plugins: [grapesjsBlocksBasic, gjsPresentWebpage, gjsForms],
       pluginsOpts: {
         grapesjsBlocksBasic: {},
         gjsPresentWebpage: {},
-        gjsForms:{}
+        gjsForms: {},
       },
     });
     setEditor(editor);
   }, []);
+
   return (
     <div className='App'>
       <div id='editor'></div>
